e7f40b57e40c4f2e3be88d04d71515d58bcb57ff
JunseoMin <minjs4562@gmail.com>
2024-04-01 08:08:26 +0900

    [Silver IV] Title: 가로수, Time: 4012 ms, Memory: 37096 KB -BaekjoonHub

--- Python/백준/Silver/2485. 가로수/가로수.py
+++ Python/백준/Silver/2485. 가로수/가로수.py
@@ -1,16 +1,16 @@
 from math import gcd
-n = int(input())
+N = int(input())
 m = int(input())
 
 ini = m
 gaps = set()
 gli = []
-for _ in range(n-1):
-    _n = int(input())
-    gap = _n - m
+for _ in range(N-1):
+    n = int(input())
+    gap = n - m
     gaps.add(gap)
     gli.append(gap)
-    m = _n
+    m = n
 
 gaps = list(gaps)
 _gcd = gaps[0]
@@ -18,8 +18,6 @@ _gcd = gaps[0]
 for g in gaps:
     _gcd = gcd(_gcd,g)
 
-ans = 0
-for _g in gli:
-    ans += _g // _gcd - 1
+cnt = ((m - ini) // _gcd) + 1
 
-print(ans)
+print(cnt - N)
