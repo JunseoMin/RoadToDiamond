2766aaa356d16e3b01d1f25fd5673fe4fd9106b8
JunseoMin <minjs4562@gmail.com>
2024-05-06 23:01:58 +0900

    [Bronze I] Title: 팰린드롬수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+while 1:
+    a = input().strip()
+    if a == "0":
+        break
+    elif a == a[::-1]:
+        print("yes")
+    else:
+        print("no")
