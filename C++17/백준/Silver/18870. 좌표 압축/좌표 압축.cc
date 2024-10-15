ba2fd9f422d8656035d9df3a48472cfc8dc920a5
JunseoMin <minjs4562@gmail.com>
2024-10-15 16:11:08 +0900

    [Silver II] Title: 좌표 압축, Time: 1592 ms, Memory: 39796 KB -BaekjoonHub

---
+++ C++17/백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+
+int main(void){
+    int N,x;
+    std::cin >> N;
+    std::vector<int> invec;
+    std::vector<int> xvec;
+    
+    for (int i = 0; i < N; i++){
+        std::cin >> x;
+        invec.push_back(x);
+        xvec.push_back(x);
+    }
+    std::sort(invec.begin(),invec.end());
+    invec.erase(std::unique(invec.begin(),invec.end()),invec.end());
+    
+    std::map<int,int> nummap;
+    
+    for(int i = 0; i < invec.size(); i ++){
+        nummap.insert(std::make_pair(invec[i],i));
+    }
+    for(const auto& n : xvec){
+        std::cout << nummap[n] << ' ';
+    }
+    
+    return 0;
+}
