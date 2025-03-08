dd89e6e2ee608c8b97529cdb88f369122a0fe094
JunseoMin <minjs4562@gmail.com>
2025-03-08 11:58:34 +0900

    [level 3] Title: N으로 표현, Time: 33.98 ms, Memory: 15.7 MB -BaekjoonHub

--- Python3/프로그래머스/3/42895. N으로 표현/N으로 표현.py
+++ Python3/프로그래머스/3/42895. N으로 표현/N으로 표현.py
@@ -1,32 +1,31 @@
 def solution(n, number):
     answer = 0
+    
     if n == number:
         return 1
     
-    answers = [[],[n]]
-    
+    dp = [[],[n]]
     for step in range(2,9):
         new = []
+        new.append(dp[step - 1][0] * 10 + n)
         
         for i in range(step):
-            for v1 in answers[i]:
-                for v2 in answers[step - i]:
-                    new.append(v1 + v2)
-                    new.append(v1 - v2)
-                    new.append(v2 - v1)
-                    new.append(v1 * v2)
+            j = step - i
+            for val in dp[i]:
+                for val2 in dp[j]:
+                    new.append(val + val2)
+                    new.append(val - val2)
+                    new.append(val2 - val)
+                    new.append(val * val2)
                     try:
-                        new.append(v1 // v2)
-                        new.append(v2 // v1)
+                        new.append(val // val2)
+                        new.append(val2 // val)
                     except:
                         pass
-            
-        new.append(int(f"{n}"*step))
+        new.append(int(f"{n}" * step))
+        new = list(set(new))
         if number in new:
-            return step
-        
-        answers.append(list(set(new)))
-        
-        
+            return step 
+        dp.append(new)
     
     return -1
--- Python3/프로그래머스/3/42895. N으로 표현/README.md
+++ Python3/프로그래머스/3/42895. N으로 표현/README.md
@@ -1,10 +1,10 @@
 # [level 3] N으로 표현 - 42895 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42895) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42895?language=python3) 
 
 ### 성능 요약
 
-메모리: 15.4 MB, 시간: 27.72 ms
+메모리: 15.7 MB, 시간: 33.98 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 05일 22:24:37
+2025년 03월 08일 11:58:31
 
 ### 문제 설명
 
