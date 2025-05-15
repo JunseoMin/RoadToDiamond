3a5229e8546f82b546806e9025575bb7bc1ba981
JunseoMin <minjs4562@gmail.com>
2025-05-15 20:58:19 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 18.83 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ Python3/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,33 +1,29 @@
 from collections import deque
-##
-def bfs(start, maps, target):
-    queue = deque([(start[0], start[1], 1)])
-    visited = set([start])
-    
-    
-    while queue:
-        x,y,d = queue.popleft()
-        
-        if (x,y) == target:
-            return d
-        
-        for dx,dy in [(1,0),(0,1),(-1,0),(0,-1)]:
-            mx = x + dx
-            my = y + dy
-            
-            if mx >= len(maps[0]) or mx < 0 or my >= len(maps) or my < 0:
-                continue
-            
-            if maps[my][mx] == 1 and (mx,my) not in visited:
-                queue.append((mx,my,d+1))
-                visited.add((mx,my))
-            
-    return -1
-    
 
 def solution(maps):
-    answer = 0    
-    answer = bfs((0,0),maps,(len(maps[0]) -1,len(maps) - 1))
+    answer = 0
+    queue = deque([(0,0,1)])
+    visited = set()
+    costs = [100000]
     
+    while queue:
+        x,y,cost = queue.popleft()
+        if (x,y) in visited:
+            continue
+        if cost > min(costs):
+            continue
+        visited.add((x,y))
+        
+        if (x,y) == (len(maps[0]) - 1, len(maps) - 1):
+            return cost
+        
+        if x+1 < len(maps[0]) and maps[y][x+1] == 1 and (x+1,y) not in visited:
+            queue.append((x+1,y,cost+1))        
+        if y+1 < len(maps) and maps[y+1][x] == 1 and (x,y+1) not in visited:
+            queue.append((x,y+1,cost+1))
+        if x-1 >= 0 and maps[y][x-1] == 1 and (x-1,y) not in visited:
+            queue.append((x-1,y,cost+1))
+        if y-1 >= 0 and maps[y-1][x] == 1 and (x,y-1) not in visited:
+            queue.append((x,y-1,cost+1))
     
-    return answer
+    return -1
