d729a9af691903b11425d0948dd1dd304fafba78
JunseoMin <minjs4562@gmail.com>
2025-10-18 08:44:50 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 13.06 ms, Memory: 10.4 MB -BaekjoonHub

--- Python3/프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ Python3/프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 18.83 ms
+메모리: 10.4 MB, 시간: 13.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 15일 20:58:15
+2025년 10월 18일 08:44:49
 
 ### 문제 설명
 
--- Python3/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ Python3/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -2,28 +2,25 @@ from collections import deque
 
 def solution(maps):
     answer = 0
+    
     queue = deque([(0,0,1)])
     visited = set()
-    costs = [100000]
+    
+    R = len(maps)
+    C = len(maps[0])
     
     while queue:
-        x,y,cost = queue.popleft()
-        if (x,y) in visited:
-            continue
-        if cost > min(costs):
-            continue
-        visited.add((x,y))
+        r,c,v = queue.popleft()
         
-        if (x,y) == (len(maps[0]) - 1, len(maps) - 1):
-            return cost
+        if (r,c) == (R-1,C-1):
+            return v
         
-        if x+1 < len(maps[0]) and maps[y][x+1] == 1 and (x+1,y) not in visited:
-            queue.append((x+1,y,cost+1))        
-        if y+1 < len(maps) and maps[y+1][x] == 1 and (x,y+1) not in visited:
-            queue.append((x,y+1,cost+1))
-        if x-1 >= 0 and maps[y][x-1] == 1 and (x-1,y) not in visited:
-            queue.append((x-1,y,cost+1))
-        if y-1 >= 0 and maps[y-1][x] == 1 and (x,y-1) not in visited:
-            queue.append((x,y-1,cost+1))
-    
+        for dr,dc in [(-1,0),(1,0),(0,-1),(0,1)]:
+            nr = r + dr
+            nc = c + dc
+            
+            if (0 <= nr < R) and (0 <= nc < C):
+                if maps[nr][nc] and (nr,nc) not in visited:
+                    visited.add((nr,nc))
+                    queue.append((nr,nc,v+1))
     return -1
