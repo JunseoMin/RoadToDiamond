18c60f1fb44faed6650101fa47b1eb8e4c23b392
JunseoMin <minjs4562@gmail.com>
2025-10-18 09:48:55 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 6.16 ms, Memory: 9.31 MB -BaekjoonHub

--- Python3/프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ Python3/프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,25 +1,36 @@
-def num_connections(node,tree,visited = []):
-    if node in visited:
-        return 0
-    cnt = 1
-    visited.append(node)
-    for nd in tree[node]:
-        cnt += num_connections(nd, tree, visited)
-    
-    return cnt
-
+from collections import defaultdict,deque
 
 def solution(n, wires):
-    answer = 200
+    answer = 123456789
     
-    for i in range(n):
-        tmp = wires[:i] + wires[i+1:]
-        tree = {i: [] for i in range(1, n + 1)}
+    for i in range(len(wires)):
+        removedWires = wires[:i] + wires[i+1:]
+        graph = defaultdict(list)
+        for s,e in removedWires:
+            graph[s].append(e)
+            graph[e].append(s)
+        for node in range(1, n + 1):
+            graph[node] = graph[node]
+        visited = set()
+        gaps = []
         
-        for v1,v2 in tmp:
-            tree[v1].append(v2)
-            tree[v2].append(v1)
-        
-        answer = min(answer, abs(2*num_connections(1, tree, []) - n))
-        
+        for start in graph.keys():
+            if start in visited:
+                continue
+            
+            visited.add(start)
+            queue = deque([start])
+            count = 0
+            
+            while queue:
+                node = queue.popleft()
+                count += 1
+                for neighbor in graph[node]:
+                    if neighbor not in visited:
+                        queue.append(neighbor)
+                        visited.add(neighbor)
+            gaps.append(count)
+                
+        answer = min(answer,abs(gaps[0] - gaps[1]))
+    
     return answer
