761133db97a08a3fa9f41c5f605e4fa79b45f19e
JunseoMin <minjs4562@gmail.com>
2025-10-21 14:46:08 +0900

    [level 2] Title: 기능개발, Time: 0.73 ms, Memory: 9.21 MB -BaekjoonHub

--- Python3/프로그래머스/2/42586. 기능개발/기능개발.py
+++ Python3/프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,21 +1,15 @@
-from math import ceil
-
 def solution(progresses, speeds):
     answer = []
-    costs = []
-    for p,s in zip(progresses, speeds):
-        cost = ceil((100 - p) / s)
-        costs.append(cost)
-    
-    curr = costs[0]
-    fin = 1
-    for i in range(1,len(costs)):
-        if curr >= costs[i]:
-            fin += 1
-        else:
-            answer.append(fin)
-            fin = 1
-            curr = costs[i]
-    answer.append(fin)
-    
+    while progresses:
+        for i in range(len(progresses)):
+            progresses[i] += speeds[i]
+        idx = -1
+        for i in range(len(progresses)):
+            if progresses[i] < 100:
+                break
+            idx = i
+        if idx != -1:
+            answer.append(idx + 1)
+            progresses = progresses[idx+1:]
+            speeds = speeds[idx+1:]
     return answer
