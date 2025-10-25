7cc2bb946c7436df47bc79f426f2bb1d8157676f
JunseoMin <minjs4562@gmail.com>
2025-10-25 10:41:11 +0900

    [level 3] Title: 네트워크, Time: 0.57 ms, Memory: 9.21 MB -BaekjoonHub

--- Python3/프로그래머스/3/43162. 네트워크/README.md
+++ Python3/프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
 
 ### 성능 요약
 
-메모리: 9.26 MB, 시간: 1.82 ms
+메모리: 9.21 MB, 시간: 0.57 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 15일 18:05:58
+2025년 10월 25일 10:41:09
 
 ### 문제 설명
 
--- Python3/프로그래머스/3/43162. 네트워크/네트워크.py
+++ Python3/프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,22 +1,32 @@
+from collections import deque
+
 def solution(n, computers):
-    answer = 0
-    visited = set()
-    graph = {i : computers[i] for i in range(len(computers))}
+    answer = n
+    graph = {}
     
     for i in range(n):
-        if i in visited:
-            continue
+        tmp = []
+        for idx,v in enumerate(computers[i]):
+            if v == 1 and idx != i:
+                tmp.append(idx)
+        graph[i] = tmp 
+    
+    visited = set()
+    print(graph)
+    
+    for i in range(n):
+        queue = deque([i])
+        visited.add(i)
+        cnt = 0
+        while queue:
+            curr = queue.popleft()
             
-        stack = [i]
+            for neighbor in graph[curr]:
+                if neighbor not in visited:
+                    queue.append(neighbor)
+                    visited.add(neighbor)
+                    cnt += 1
         
-        while stack:
-            node = stack.pop()
-            visited.add(node)
-            
-            for i in range(len(graph[node])):
-                isNeighbor = graph[node][i]
-                if isNeighbor and not(i in visited):
-                    stack.append(i)
-        answer += 1
-        
+        answer -= cnt
+    
     return answer
